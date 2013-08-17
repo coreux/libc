d2acdac59f1165af8b5309ce2702bfc64348062c
Mo McRoberts <mo@nevali.net>
2013-08-17 23:44:12 +0100

    Initial commit.

---
+++ p_libc.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2013 Mo McRoberts.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef P_LIBC_H_
+# define P_LIBC_H_                     1
+
+# include <ux/cdefs.h>
+
+extern const char ux_libc_ident[] __UX_PRIVATE(libc_ident);
+
+#endif
